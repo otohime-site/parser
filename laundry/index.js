a4db9e7303decaeee72dd110a1f735346a692b18
Hsiao-Ting Yu <sst.dreams@gmail.com>
2018-04-28 03:15:19 +0800

    Fix eslint errors.

--- laundry/index.js
+++ laundry/index.js
@@ -1,24 +1,24 @@
+/* global fetch:false */
 const cheerio = require('react-native-cheerio');
 
-module.exports = async function (progress) {
+module.exports = async (progress) => {
   const result = {};
   const difficulties = ['easy', 'basic', 'advanced', 'expert', 'master', 'remaster'];
 
   // Get the main page.
-  var res = await fetch('https://maimai-net.com/maimai-mobile/home/', { credentials: 'same-origin' });
-  var $ = cheerio.load(await res.text());
-  if ($('.status_data').length < 1) {
+  const rootRes = await fetch('https://maimai-net.com/maimai-mobile/home/', { credentials: 'same-origin' });
+  const $r = cheerio.load(await rootRes.text());
+  if ($r('.status_data').length < 1) {
     throw new Error('Cannot get user data!');
   }
-  result.cardName = $('.status_data div:first-child a').text();
-  result.title = $('.status_data div:nth-child(2) span').text();
-  const rawRating = $('.status_data div:nth-child(3) span').text();
+  result.cardName = $r('.status_data div:first-child a').text();
+  result.title = $r('.status_data div:nth-child(2) span').text();
+  const rawRating = $r('.status_data div:nth-child(3) span').text();
   const ratingFound = rawRating.match(/([0-9]+\.[0-9]+)[^0-9]+([0-9]+\.[0-9]+)/);
-  result.rating = ratingFound[1];
-  result.maxRating = ratingFound[2];
-  result.class = $('.f_r img').attr('src').split('/').pop()
-    .match(/[0-9]+\_[0-9]+/)[0];
-  await progress(1 / (difficulties.length + 1) * 100);
+  [result.rating, result.maxRating] = ratingFound;
+  [result.class] = $r('.f_r img').attr('src').split('/').pop()
+    .match(/[0-9]+_[0-9]+/);
+  await progress(100 / (difficulties.length + 1));
 
   // Get all song results.
   const body = new URLSearchParams();
@@ -28,33 +28,35 @@ module.exports = async function (progress) {
   body.set('x', '50');
   body.set('y', '10');
   result.scores = [];
-  for (var d = 0; d < difficulties.length; d++) {
+  for (let d = 0; d < difficulties.length; d += 1) {
     const difficulty = difficulties[d];
-    var res = await fetch(`https://maimai-net.com/maimai-mobile/music/${difficulty}Genre/`, {
+    // eslint-disable-next-line no-await-in-loop
+    const res = await fetch(`https://maimai-net.com/maimai-mobile/music/${difficulty}Genre/`, {
       method: 'POST', body, credentials: 'same-origin',
     });
-    var $ = cheerio.load(await res.text());
-    var category = '';
+    // eslint-disable-next-line no-await-in-loop
+    const $ = cheerio.load(await res.text());
+    let category = '';
     if ($('#accordion').length < 1) {
       throw new Error('Cannot get score data!');
     }
-    $('#accordion > div, #accordion > h3').each(function () {
-      if (this.tagName.search(/^DIV$/i) != -1) {
-        category = $(this).find('span').text();
+    $('#accordion > div, #accordion > h3').each(() => {
+      const $this = $(this);
+      if (this.tagName.search(/^DIV$/i) !== -1) {
+        category = $this.find('span').text();
         return;
       }
-      const $this = $(this);
       const score = {};
       score.category = category;
       score.difficulty = d;
       const $title = $this.find('div:first-child');
       if ($title.length > 0) {
         score.songName = $this.find('div:first-child').text();
-        score.score = $this.find('.achievement').text().match(/[0-9]+\.[0-9]+/)[0];
+        [score.score] = $this.find('.achievement').text().match(/[0-9]+\.[0-9]+/);
         const flags = [];
-        $this.find('.text_r img').each(function () {
+        $this.find('.text_r img').each(() => {
           const src = $(this).attr('src');
-          const srcFound = src.match('(fc_silver|fc_gold|ap|100)\.png');
+          const srcFound = src.match(/(fc_silver|fc_gold|ap|100)\.png/);
           if (srcFound) {
             flags.push(srcFound[1]);
           }
@@ -71,6 +73,7 @@ module.exports = async function (progress) {
       score.songId = parseInt($next.find('input[name=musicId]').val(), 10);
       result.scores.push(score);
     });
+    // eslint-disable-next-line no-mixed-operators, no-await-in-loop
     await progress((2 + d) / (difficulties.length + 1) * 100);
   }
   return result;
